c11883ae44d2e7b06003dc6d6bc43c780a8f5926
alisharifipx <134028644+alisharifipx@users.noreply.github.com>
2024-11-20 22:51:27 +1030

    Add 16x16 grid

---
+++ index.js
@@ -0,0 +1,18 @@
+function createGrid(size) {
+    const container = document.querySelector('div.container');
+    
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        for (let j = 0; j < size; j++) {
+            const col = document.createElement('div');
+            col.setAttribute('class','col');
+            col.style.border = "1px solid black";
+            col.textContent = (j * size) + i;
+            row.appendChild(col);
+        }
+        container.appendChild(row);
+    }
+}
+
+createGrid(16);
